cbb6feb3804083abdedec8198ad1b1a753316aa1
AkimbaevTimyr <60063664+AkimbaevTimyr@users.noreply.github.com>
2022-08-15 09:58:02 +0300

    commit

--- src/components/pages/Main/Main.tsx
+++ src/components/pages/Main/Main.tsx
@@ -5,17 +5,29 @@ import PersonalFilms from './PersonalFilms/PersonalFilms'
 import PersonalSeries from './PersonalSeries/PersonalSeries'
 import UpcomingPremiers from './UpcomingPremires/UpcomingPremiers'
 import styles from './style.module.css'
+import { Link } from 'react-router-dom'
 
 const Main: FC = () => {
   const {isLoading} = useAppSelector(state => state.movies)
   if(isLoading){
     return <Loading />
   }
+  const navigation = [
+    { name: 'Главная', href: '/', },
+    { name: 'Избранное', href: '/watchlist', },
+    { name: 'Фильмы', href: '/films', },
+    { name: 'Сериалы', href: '/tvshows', },
+]
   return (
    <div className={styles.main}>
     <PersonalFilms />
     <PersonalSeries />
     <UpcomingPremiers />
+    <div className={styles.nav}>
+                    {navigation.map((el: any) => (
+                        <Link to={el.href}><div className={styles.navItem}>{el.name}</div></Link>
+                    ))}
+                </div>
    </div>
   )
 }
--- src/components/pages/Main/PersonalFilms/PersonalFilms.tsx
+++ src/components/pages/Main/PersonalFilms/PersonalFilms.tsx
@@ -29,7 +29,6 @@ const PersonalFilms: FC = () => {
                     </Link>)
                 ))}
             </div>
-
         </div>
     )
 }
