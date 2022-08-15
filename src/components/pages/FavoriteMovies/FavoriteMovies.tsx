d779cd84055567ceeddb790c38964d1c1403a8b2
AkimbaevTimyr <60063664+AkimbaevTimyr@users.noreply.github.com>
2022-08-15 11:13:52 +0300

    commit

--- src/components/pages/FavoriteMovies/FavoriteMovies.tsx
+++ src/components/pages/FavoriteMovies/FavoriteMovies.tsx
@@ -3,7 +3,8 @@ import { useAppDispatch, useAppSelector } from '../../../hooks/redux'
 import {getFavoriteMovies } from '../../../store/actions/MovieActionCreator'
 import FilmItem from '../../FilmItem/FilmItem'
 import Loading from '../../Loading/loading'
-import './style.css'
+import styles from './style.module.css'
+
 const WatchList: FC = () => {
   const email = localStorage.getItem('email')
   const {favoriteMovies} = useAppSelector(state => state.movies)
@@ -12,7 +13,7 @@ const WatchList: FC = () => {
     dispatch(getFavoriteMovies(email))
   },[])
   return (
-    <div className='items'>
+    <div className={styles.items}>
       {favoriteMovies.length == 0 ? <Loading /> : (favoriteMovies.map((el: any) =>(
         <FilmItem  key={el.id} id={el.id} img={el.poster_path} title={el.original_name || el.title} vote_average={el.vote_average} release_date={el.first_air_date || el.release_date} type={el.type}/>
       )))}
