7697a32b6cc29a2eb63c7756c6c028f76bc029bf
AkimbaevTimyr <60063664+AkimbaevTimyr@users.noreply.github.com>
2022-08-18 17:33:39 +0300

    Updates

--- src/helpers/convertTimestampToDate/convertTimestampToDate.ts
+++
@@ -1,7 +0,0 @@
-import dayjs from "dayjs";
-import 'dayjs/locale/ru'
-dayjs.locale('ru')
-
-export function convertTimestampToDate(date: string | undefined){
-    return dayjs(date).format("D MMMM YYYY")
-}
